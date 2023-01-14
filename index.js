8191947b62e9ae7aa9747aa0f4081c09ca6a1aa5
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-01-14 12:21:40 +0530

    change index.ejs and index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const PORT = 2000 || process.env.PORT;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
@@ -11,6 +18,14 @@ app.get('/cpp',(req,res)=>{
 });
 app.get('/dsa',(req,res)=>{
     res.render('dsa');
+});
+app.post('/feedback',(req,res,next)=>{
+    let name = {
+       name:req.body.name,
+    };
+    res.redirect('/');
+    next();
+    console.log(name);
 })
 
 
